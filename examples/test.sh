8634f7bf30e49fa2d7b45448fd719e2522d3d9e3
Yuxin Ma <mayuxin98@gmail.com>
2024-10-04 12:16:52 -0400

    Add test.sh

---
+++ examples/test.sh
@@ -0,0 +1,11 @@
+dataset="rel-f1"
+tasks=("driver-position" "driver-top3" "driver-dnf")
+
+for task in "${tasks[@]}"; do
+    for outer_aggr in "${outer_aggrs[@]}"; do
+        for i in {1..10}; do
+            echo "Running gnn_node.py with dataset=${dataset}, task=${task}, outer_aggr=${outer_aggr}"
+            CUDA_VISIBLE_DEVICES=0 python gnn_node.py --wandb True --wandb_gradient False --epoch 50 --dataset "$dataset" --task "$task" --outer_aggr "sum" --wandb_name "$dataset$task$"
+        done
+    done
+done
